33e0516e7d806e7216cfeaacd330760d53c0b09d
David Cruwys <david@ideasmen.com.au>
2022-01-01 12:40:26 +1100

    update ruby/components/configuration

--- ruby/components/configuration/.builders/configuration_director.rb
+++ ruby/components/configuration/.builders/configuration_director.rb
@@ -75,7 +75,7 @@ class ConfigurationDirector
       dom: dom,
       cop: true,
       on_exist: on_exist,
-      template_file: "ruby/components/configuration/#{dom[:style]}_configuration.rb")
+      template_file: "ruby/components/configuration/#{dom[:style]}_config.rb")
 
     builder.add_file(file, **opts)
   end
